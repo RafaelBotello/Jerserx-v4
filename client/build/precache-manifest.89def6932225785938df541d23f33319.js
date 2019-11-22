b6ec38cdc0c5b0115cc91c514425a491b95c1beb
Rafael Botello <franciscobotello@Administrators-MacBook-Pro.local>
2019-11-21 19:57:50 -0500

    Initial commit.

---
+++ client/build/precache-manifest.89def6932225785938df541d23f33319.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cf43c179f1ca6736f4493bdb3313a19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b61a39fe4c559adae3fe",
+    "url": "/static/css/main.d12b5816.chunk.css"
+  },
+  {
+    "revision": "0956a8d90751cc9f5bfa",
+    "url": "/static/js/2.bf5c5b4a.chunk.js"
+  },
+  {
+    "revision": "b61a39fe4c559adae3fe",
+    "url": "/static/js/main.4492e6d4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "300205b5bef11bffeb913babb89835a0",
+    "url": "/static/media/img1.300205b5.jpg"
+  },
+  {
+    "revision": "ab4def5471b94d15c331b83f4a647bce",
+    "url": "/static/media/img2.ab4def54.jpg"
+  },
+  {
+    "revision": "b8fde7535de517b2801bddb32c4f29e8",
+    "url": "/static/media/img3.b8fde753.jpg"
+  },
+  {
+    "revision": "108c1fcd40a6884f5b271ffa54a0ecac",
+    "url": "/static/media/img4.108c1fcd.jpg"
+  },
+  {
+    "revision": "60875a87b9590aa95c6ecbabb42a1800",
+    "url": "/static/media/img5.60875a87.jpg"
+  },
+  {
+    "revision": "878d885986bff3035f55b97d13c4437e",
+    "url": "/static/media/slide1.878d8859.jpg"
+  },
+  {
+    "revision": "4a2ea0dceee7b6adbcc581da546bc683",
+    "url": "/static/media/slide2.4a2ea0dc.jpg"
+  },
+  {
+    "revision": "b1d47a3b0ea4743494514e92c374b2c6",
+    "url": "/static/media/thanks.b1d47a3b.jpg"
+  }
+]);
